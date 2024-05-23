f5efcfc84b428a1e53845e2d7d19772f52e7a0b4
0zodbek <ozpdbekov@gmail.com>
2024-05-24 00:54:14 +0500

    9 ta 6 yoq

---
+++ app.js
@@ -0,0 +1,123 @@
+// 1-masala
+
+// alert("poyezd ketishi kerak bolgan vaqtdan kechikdi bu vaqtni kiriting");
+
+// let t = 24;
+
+// let T = 1000;
+
+// let g = (t+T)%24;
+
+// let a = +prompt("a");
+
+// console.log(g);
+
+
+
+// 2-masala
+
+// let C = +prompt('haroratni c da kiriting;');
+
+// let = k;
+
+// let = f;
+
+// k = C + 273.15;
+
+// k = C * 1.80 + 32.00;
+
+// console.log(k);
+
+// console.log(f);
+
+
+// 3-masala
+
+// alert("bilmasvoy ukalarinig yoshiga bogliq masala");
+
+// let v = +prompt("v");
+
+// let a = +prompt("a");
+
+// let b = +prompt("b");
+
+// let g = (b-v);
+
+// let k = (a-v);
+
+// console.log(g);
+
+// console.log(k);
+
+
+// 4-masala
+
+// alert("Xakimbekning tarvuzi quyoshda qolib qancha bolib qolganini aniqlang");
+
+// let n = +prompt("n");
+
+// let a = +prompt("a");
+
+// let b = (n % a);
+
+// console.log(b);
+
+
+// 5-masala
+
+// alert("Tomonlari soni N ga teng bo’lgan qavariq ko’pburchakning ichki burchaklari yig’indisini toping");
+
+// let n = +prompt("n");
+
+// let b = n * (n-2); 
+
+// console.log(b);
+
+
+// 6-masala
+
+// alert("Sizga biror vaqtni beraman shu vaqtda t minut o'tgandan keyingi vaqtni ayting. 24 soatlik vaqt haqida malumotga ega bo'lmasangiz;");
+
+// let a = +prompt();
+
+
+// 7-masala
+
+// alert("sardor buvisini yoshini yarmiga qachon yetishini aniqlovchi algoritm;");
+
+// let b = +prompt("b");
+
+// let s =  +prompt("s");
+
+// let y = (b-s);
+
+// let h = (y/2)
+
+// console.log(h);
+
+
+// 8-masala
+
+// alert("o'rtacha maoshni topish uchun algoritm;");
+
+// let max = +prompt("max");
+
+// let min =  +prompt("min");
+
+// let ort = (max+min)/2;
+
+// console.log(ort);
+
+
+// 9-masala
+
+// alert("Sizga n burchakli qavariq ko'p burchak berilgan. Uning dioganallari jami nechta nuqatada kesishishini toping;");
+
+// let n = +prompt("n");
+
+// console.log(n * (n-3)/2);
+
+
+// 10-masala
+
+// ...AbortController
